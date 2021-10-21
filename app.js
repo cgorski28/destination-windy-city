979004b7817092b11f8507619be562ec29aa9f21
Cole Gorski <gorskicole@gmail.com>
2021-10-20 23:33:21 -0500

    add reviews model

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ const express = require('express');
 const path = require('path')
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
-const { campgroundSchema } = require('./schemas');
+const { campgroundSchema , reviewSchema } = require('./schemas');
 const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError')
 const Campground = require('./models/campground.js')
+const Review = require('./models/review')
 
 const methodOverride = require('method-override')
 
@@ -33,6 +34,16 @@ app.use(methodOverride('_method'))
 
 const validateCampground = (req, res, next) => {
     const { error } = campgroundSchema.validate(req.body);
+    if(error){
+        const msg = error.details.map(el => el.message).join(',');
+        throw new ExpressError(msg, 400)
+    } else {
+        next();
+    }
+}
+
+const validateReview = (req, res, next) => {
+    const {error} = reviewSchema.validate(req.body);
     if(error){
         const msg = error.details.map(el => el.message).join(',');
         throw new ExpressError(msg, 400)
@@ -83,6 +94,15 @@ app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
     const deletedCampground = await Campground.findByIdAndDelete(id);
     console.log(deletedCampground)
     res.redirect("/campgrounds")
+}))
+
+app.post('/campground/:id/review', validateReview, catchAsync(async(req, res) => {
+    const campground = await Campground.findById(req.params.id)
+    const review = new Review(req.body.review);
+    campground.reviews.push(review);
+    await review.save()
+    await campground.save()
+    res.redirect(`/campgrounds/${campground._id}`);
 }))
 
 app.all('*', (req, res, next) => {
