73dc9c61da99deaf7657994f5ebc778505ffc889
Cole Gorski <gorskicole@gmail.com>
2021-11-13 17:34:01 -0600

    add beginning portions of image upload

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -3,11 +3,17 @@ const router = express.Router();
 const campgrounds = require('../controllers/campgrounds')
 const catchAsync = require('../utils/catchAsync');
 const { isLoggedIn, isAuthor, validateCampground } = require('../middleware')
+const multer = require('multer')
+const { storage } = require('../cloudinary')
+const upload = multer({ storage })
 
 
 router.route('/')
     .get(catchAsync(campgrounds.index))
-    .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground))
+    //.post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground))
+    .post(upload.array('image'), (req, res) => {
+        res.send(req.files)
+    })
 
 router.get('/new', isLoggedIn, campgrounds.renderNewForm)
 
