b8dfd9e3179f9c2171ca6b4f2b6b5a7f2642963e
Cole Gorski <gorskicole@gmail.com>
2021-11-19 08:39:47 -0600

    update port

--- app.js
+++ app.js
@@ -108,7 +108,7 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('error', { err })
 })
 
-const port  = process.env.port || 3000;
+const port  = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`serving on port ${port}`)
 })
