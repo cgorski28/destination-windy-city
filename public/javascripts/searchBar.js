b50df353bf866ffcb2de8f038bc6ddf570531389
Cole Gorski <gorskicole@gmail.com>
2021-11-26 09:12:55 -0600

    add search bar

---
+++ public/javascripts/searchBar.js
@@ -0,0 +1,15 @@
+const searchBar = document.forms[0].querySelector('input')
+
+searchBar.addEventListener('keyup', function(e){
+    console.log('search term: ', e.target.value)
+    const term = e.target.value.toLowerCase()
+    const allCards = document.querySelectorAll('.card')
+    Array.from(allCards).forEach(function(item){
+        const title = item.querySelector('h5').textContent
+        if(title.toLowerCase().indexOf(term) === -1){
+            item.style.display='none'
+        } else {
+            item.style.display = 'block'
+        }
+    })
+})
