2bc1c0db5d3eaa86211ef5749183dbac88201e24
Cole Gorski <gorskicole@gmail.com>
2021-12-03 00:16:53 -0600

    autocomplete for address and new listings

---
+++ public/javascripts/autocomplete.js
@@ -0,0 +1,23 @@
+let autocomplete;
+function initAutocomplete(){
+    autocomplete = new google.maps.places.Autocomplete(
+        document.getElementById('attraction'),
+        {
+            types: ['establishment'], 
+            componentRestrictions: {'country': ['US']},
+            fields: ['name', 'formatted_address', 'price_level']
+        }
+    );
+
+    autocomplete.addListener('place_changed', onPlaceChanged);
+}
+
+function onPlaceChanged(){
+    var place = autocomplete.getPlace();
+    if(!place.name){
+        document.getElementById('attraction').placeholder = 'Enter a place'
+    } else {
+        document.getElementById('attraction').value = place.name;
+        document.getElementById('location').setAttribute('value', place.formatted_address);
+    }
+}
